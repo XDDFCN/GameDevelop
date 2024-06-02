037730f1239616a17ba76a200e172355d15ee74b
XDDFCN <151542368+XDDFCN@users.noreply.github.com>
2024-06-02 16:20:09 +0800

    球球有方向版

--- FlipFootballGame .py
+++ FlipFootballGame .py
@@ -10,14 +10,18 @@ gameCanvas.pack()
 
 ovalID = gameCanvas.create_oval(30,40,10,20,fill="#FF5687",outline="orange",width=3)
 
+moveX = random.random() * 10 - 5  # -40，40
+moveY = random.random() * 10 - 5
+
+
 while True:
-    moveX = random.random()*80-40   #-40，40
-    moveY = random.random()*80-40
     ballCoord = gameCanvas.coords(ovalID)
-    if ballCoord[0]+moveX>=0 and ballCoord[1]+moveY<=500 and ballCoord[2]+moveX<=500 and ballCoord[3]+moveY>=0:
+    if ballCoord[0]+moveX>0 and ballCoord[1]+moveY<500 and ballCoord[2]+moveX<500 and ballCoord[3]+moveY>0:
         gameCanvas.move(ovalID,moveX,moveY)
         mainWindow.update()
-        time.sleep(0.1)   #小球每移动一下停留的秒数
+        time.sleep(0.01)   #小球每移动一下停留的秒数
+    elif ballCoord[0]+moveX<=0 or ballCoord[1]+moveY>=500 or ballCoord[2]+moveX>=500 or ballCoord[3]+moveY<=0:
+        moveX = random.random() * 10 - 5  # -40，40
+        moveY = random.random() * 10 - 5
 
-
-# mainWindow.mainloop()   #可有可无，挑不出while True循环
+# mainWindow.mainloop()   #可有可无，跳不出while True循环
